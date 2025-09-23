d45ca976b4c6df91ecedd57f3eb93d01de0baed4
D-Jensen1 <dominic.jensen1@outlook.com>
2025-09-23 09:50:12 -0700

    Added WalletLibrary

--- Week5/week5/LearnClassModeling/WalletTest.cs
+++ Week5/week5/LearnClassModeling/WalletTest.cs
@@ -1,6 +1,8 @@
 using LearnClassModeling.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -105,7 +107,116 @@ namespace LearnClassModeling
                     test += bill.Amount;
                 }
         */
+
         [TestMethod]
+        public void BillCounterTest()
+        {
+            Wallet w = new();
+            var one = (new Bill(Denomination.One));
+            var five = (new Bill(Denomination.Five));
+            var ten = (new Bill(Denomination.Ten));
+
+            w.AddBill(ten);
+            w.AddBill(ten);
+            w.AddBill(ten);
+            w.AddBill(five);
+            w.AddBill(five);
+            w.AddBill(one);
+            w.AddBill(one);
+
+            Assert.AreEqual(2, w[one]);
+            Assert.AreEqual(2, w[five]);
+            Assert.AreEqual(3, w[ten]);
+
+        }
+
+        [TestMethod]
+        public void DemoLogger()
+        {
+            Wallet w = new();
+
+            w.BillAdded += (w, b) => Debug.WriteLine($"{b} is added to wallet");
+            w.BillRemoved += (w, b) => Debug.WriteLine($"{b} is removed from wallet");
+
+            var one = (new Bill(Denomination.One));
+            var five = (new Bill(Denomination.Five));
+            var ten = (new Bill(Denomination.Ten));
+
+            w.AddBill(ten);
+            w.AddBill(ten);
+            w.AddBill(ten);
+            w.AddBill(five);
+            w.AddBill(five);
+            w.AddBill(one);
+            w.AddBill(one);
+            w.RemoveBill(five);
+            w.RemoveBill(one);
+        }
+
+        [TestMethod]
+        public void VerifiesWalletAmountIsCorrectUsingEventSubscribtion()
+        {
+            Wallet w = new();
+            int trackingTotal = 0;
+
+            w.BillAdded += (wallet, b) => trackingTotal += b.Amount;
+            w.BillRemoved += (wallet, b) => Debug.WriteLine($"{b.Amount} is removed from wallet. You now have {w.BillTotal:C0}.");
+            w.BillRemoved += (wallet, b) => trackingTotal -= b.Amount;
+
+            var one = (new Bill(Denomination.One));
+            var five = (new Bill(Denomination.Five));
+            var ten = (new Bill(Denomination.Ten));
+
+            w.AddBill(ten);
+            w.AddBill(ten);
+            w.AddBill(ten);
+            w.AddBill(five);
+            w.AddBill(five);
+            w.AddBill(one);
+            w.AddBill(one);
+            w.RemoveBill(five);
+            w.RemoveBill(one);
+
+            Assert.AreEqual(36, trackingTotal);
+        }
+
+        [TestMethod]
+        public void ReadOnlyPropertyIsntReadOnly()
+        {
+            Wallet w = new();
+
+            var one = (new Bill(Denomination.One));
+            var five = (new Bill(Denomination.Five));
+            var ten = (new Bill(Denomination.Ten));
+
+            w.AddBill(ten);
+            w.AddBill(ten);
+            w.AddBill(ten);
+            w.AddBill(five);
+            w.AddBill(five);
+            w.AddBill(one);
+            w.AddBill(one);
+
+            //w.Bills = new(); // this will be stopped by compiler because the property is read only
+            var bills = w.Bills;
+
+            Stack<ImmutableList<Bill>> history = new();
+
+            var newBills = bills.Remove(five);
+            history.Push(newBills);
+            Assert.IsTrue(bills.Count == 7);
+            Assert.IsTrue(newBills.Count == 6);
+
+            newBills = newBills.Add(ten);
+            history.Push(newBills);
+            Assert.IsTrue(newBills.Count == 7);
+
+            newBills = newBills.Add(one);
+            history.Push(newBills);
+            Assert.IsTrue(newBills.Count == 8);
+        }
+
+
         public void GenericDelegateTest()
         {
             // Final parameter of Func<> is the return type
@@ -121,10 +232,5 @@ namespace LearnClassModeling
             Assert.AreEqual("8", tester.Invoke(5, 3));
         }
 
-        [TestMethod]
-        public void DictionaryIndexTest()
-        {
-
-        }
     }
 }
